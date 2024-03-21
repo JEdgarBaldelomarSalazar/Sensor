6293dfe4079343e949c85959cc6eef15835dbc47
PROEX\edgar.baldelomar <edgar.baldelomar@leoproex.com>
2024-03-21 14:17:31 +0100

    final commit

---
+++ Sensor/Program.cs
@@ -0,0 +1,21 @@
+﻿using Sensor;
+using WebSocketSharp.Server;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Starting server");
+            WebSocketServer server = new WebSocketServer("ws://127.0.0.1:9000");
+            server.AddWebSocketService<SendData>("/Sensor");
+            server.Start();
+            Console.ReadKey();
+            server.Stop();
+        }
+        
+    }
+
+}
---
+++ Sensor/SendData.cs
@@ -0,0 +1,96 @@
+﻿/*using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Sensor
+{
+    public class SendData : WebSocketBehavior
+    {
+        private readonly int _periodDefault = 5;
+        private readonly int _convertMs = 1000;
+        private int _period;
+        private Random _rnd;
+        public SendData()
+        {
+            _period = _periodDefault;
+            _rnd = new Random();
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+
+            Console.WriteLine($"Received message from client: {e.Data}");
+
+            if (int.TryParse(e.Data, out int period))
+            {
+                _period = period;
+
+                Task<double> tempTask = Generate();
+                tempTask.ContinueWith(task =>
+                {
+                    string tempString = task.Result.ToString();
+
+                    Sessions.Broadcast(tempString);
+                });
+            }
+
+        }
+        private async Task<double> Generate()
+        {
+            await Task.Delay(_period * _convertMs);
+            double temp = _rnd.NextDouble() * (30 - 15) + 15;
+            return temp;
+        }
+    }
+}
+*/
+
+using System;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Sensor
+{
+    public class SendData : WebSocketBehavior
+    {
+        private readonly int _periodDefault = 5;
+        private readonly int _convertMs = 700;
+        private int _period;
+        private Random _rnd;
+        private bool _isPeriodSet = false;
+
+        public SendData()
+        {
+            _period = _periodDefault;
+            _rnd = new Random();
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine($"Received message from client: {e.Data}");
+
+            if (!_isPeriodSet && int.TryParse(e.Data, out int period))
+            {
+                lock (this) 
+                {
+                    _period = period;
+                    _isPeriodSet = true;
+                    Task.Run(() => GenerateAndSendData());
+                }
+            }
+        }
+
+        private async Task GenerateAndSendData()
+        {
+            while (_isPeriodSet)
+            {
+                double temp = _rnd.NextDouble() * (30 - 15) + 15;
+                //Sessions.Broadcast(temp.ToString());
+                Send(temp.ToString());  
+                Console.WriteLine($"Waiting for {_period * _convertMs} milliseconds");
+                await Task.Delay(_period * _convertMs);
+            }
+        }
+    }
+}
